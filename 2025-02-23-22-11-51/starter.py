1957f4345230b503fe2f4e6cf208c2327f06b98b
Roshan Thapa <roshanrays236@gmail.com>
2025-02-27 10:04:32 +0545

    Adding SUMO Files

---
+++ 2025-02-23-22-11-51/starter.py
@@ -0,0 +1,16 @@
+import traci 
+import traci.constants as tc 
+
+# Specify "--num-clients" --> "2" for two users . User 1 is Client 1 and User 2 is Client 2, and so on
+traci.start([
+    "sumo-gui",
+    "-c", "/Users/roshanthapa/Sumo/2025-02-23-22-11-51/osm.sumocfg", \
+    "--num-clients", "2"], port=8813)
+
+# Client 1 is always this server. Set to 1
+traci.setOrder(1)
+
+while traci.simulation.getMinExpectedNumber() > 0:
+    traci.simulationStep()
+    # More traci commands
+traci.close()
